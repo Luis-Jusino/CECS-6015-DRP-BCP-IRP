46f3e6551ff23584e14bcae7a10d633cc84ca263
Luis Jusino <luis.f.jusino@gmail.com>
2025-08-25 20:38:21 -0400

    Added vite.config.js

--- vue-drp-bcp-irp/vue-drp-bcp-irp/vite.config.js
+++ vue-drp-bcp-irp/vue-drp-bcp-irp/vite.config.js
@@ -1,3 +1,6 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-export default defineConfig({ plugins: [vue()] })
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+export default defineConfig({
+  plugins: [vue()],
+  base: "luis-jusino.github.io", // <-- add this line for project sites
+});
