4bc8f69c2c4e1cf98c19a878b089a2642f64ddb8
Luis Jusino <luis.f.jusino@gmail.com>
2025-08-25 21:08:49 -0400

    Trying to fix GitHub Pages deployment issue

--- vue-drp-bcp-irp/vue-drp-bcp-irp/vite.config.js
+++ vue-drp-bcp-irp/vue-drp-bcp-irp/vite.config.js
@@ -2,5 +2,5 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 export default defineConfig({
   plugins: [vue()],
-  base: "CECS-6015-DRP-BCP-IRP", // <-- add this line for project sites
+  base: "/CECS-6015-DRP-BCP-IRP/", // <-- add this line for project sites
 });
